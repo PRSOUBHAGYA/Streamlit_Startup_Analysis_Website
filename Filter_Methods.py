90f95593f7c21cbc5f857e4ac38de3967c0af009
PR_SOUBHAGYA <prsoubhagyaranjan1997@gmail.com>
2023-01-02 21:34:05 +0530

    Overall Page modified

---
+++ Filter_Methods.py
@@ -0,0 +1,109 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def display_overall_analysis(df):
+    col1,col2,col3,col4 = st.columns(4)
+    with col1:
+        st.metric("Total Amount Investment",str(round(df['Amount'].sum()))+" Cr.")
+    with col2:
+        st.metric("Max Amount Invested",str(round(df.groupby('Startup')['Amount'].sum().sort_values(ascending=False).head(1).values[0]))+" Cr.")
+    with col3:
+        st.metric("Average Amount Invested",str(round(df.groupby('Startup')['Amount'].sum().mean()))+" Cr.")
+    with col4:
+        st.metric("Total Startups",df['Startup'].nunique())
+    st.title("")
+    st.subheader("Month By Month Investments")
+    temp_df = df.groupby(['Year', 'Month'])['Amount'].sum().reset_index()
+    temp_df['X-Axis'] = temp_df['Year'].astype(str) + "-" + temp_df['Month'].astype('str')
+    st.line_chart(temp_df, x='X-Axis', y='Amount')
+    col1, col2 = st.columns(2)
+    with col1:
+        sector_with_highamount = df.groupby('Vertical')['Amount'].sum().sort_values(ascending=False).head()
+        st.subheader('Top 5 invested Sectors Basis of Amounts')
+        fig,ax = plt.subplots()
+        ax.pie(sector_with_highamount,labels=sector_with_highamount.index,autopct="%0.01f%%")
+        st.pyplot(fig)
+    with col2:
+        sector_with_total_investments = df.groupby('Vertical')['Startup'].count().sort_values(ascending=False).head(10)
+        st.subheader('Top 10 invested Sectors Basis of Startups')
+        fig1, ax1 = plt.subplots()
+        ax1.pie(sector_with_total_investments, labels=sector_with_total_investments.index, autopct="%0.01f%%")
+        st.pyplot(fig1)
+    st.title("")
+    col1, col2 = st.columns(2)
+    with col1:
+        yearwise_amount_investments = df.groupby('Year')['Amount'].sum()
+        st.subheader("Investment amounts per Year")
+        fig2, ax2 = plt.subplots()
+        ax2.bar(yearwise_amount_investments.index, yearwise_amount_investments.values)
+        st.pyplot(fig2)
+    with col2:
+        yearwise_nvestments_in_startup = df.groupby('Year')['Startup'].count()
+        st.subheader("Total startups investment per Year")
+        fig3, ax3 = plt.subplots()
+        ax3.bar(yearwise_nvestments_in_startup.index, yearwise_nvestments_in_startup.values)
+        st.pyplot(fig3)
+    st.title("")
+    col1, col2 = st.columns(2)
+    with col1:
+        st.write("")
+        st.subheader("Top 10 Investors in startups")
+        st.dataframe(df.groupby('Investors')['Amount'].sum().sort_values(ascending=False).head((10)))
+    with col2:
+        st.write("")
+        st.subheader("Top 10 Investors in startups")
+        st.dataframe(df.groupby('Startup')['Amount'].sum().sort_values(ascending=False).head((10)))
+    st.title("")
+    col1, col2 = st.columns(2)
+    with col1:
+        st.subheader("Top 10 InvestTypes in Startups")
+        top10_investment_types = df.groupby('Investment Type')['Startup'].count().sort_values(ascending=False).head(10)
+        st.bar_chart(top10_investment_types)
+    with col2:
+        st.subheader("Top 10 Cities invested")
+        top_city_invested = df.groupby('City')['Startup'].count().sort_values(ascending=False).head(10)
+        st.bar_chart(top_city_invested)
+
+
+
+def display_investor_info(df,name):
+    st.header(name+" Investment Details in startups")
+    st.subheader("Recent Investments")
+    st.dataframe(df[df['Investors'].str.contains(name)][['Date','Startup','Vertical','City','Amount']].head(5))
+    col1, col2 = st.columns(2)
+    with col1:
+        investment_sectors = df[df['Investors'].str.contains(name)].groupby('Vertical')['Amount'].sum()
+        st.subheader('Investments Sectors')
+        fig, ax = plt.subplots()
+        ax.pie(investment_sectors, labels=investment_sectors.index,autopct="%0.01f%%")
+        st.pyplot(fig)
+    with col2:
+        biggest_investments = df[df['Investors'].str.contains(name)].groupby('Startup')['Amount'].sum().sort_values(ascending=False).head()
+        st.subheader("Biggest Investments")
+        fig1, ax1 = plt.subplots()
+        ax1.bar(biggest_investments.index, biggest_investments.values)
+        st.pyplot(fig1)
+
+    col1, col2 = st.columns(2)
+    with col1:
+        investment_types = df[df['Investors'].str.contains(name)].groupby('Investment Type')['Amount'].sum()
+        st.subheader('Investments Rounds')
+        fig2, ax2 = plt.subplots()
+        ax2.pie(investment_types, labels=investment_types.index, autopct="%0.01f%%")
+        st.pyplot(fig2)
+    with col2:
+        investment_cities = df[df['Investors'].str.contains(name)].groupby('City')['Amount'].sum()
+        st.subheader('Investments City')
+        fig3, ax3 = plt.subplots()
+        ax3.pie(investment_cities, labels=investment_cities.index, autopct="%0.01f%%")
+        st.pyplot(fig3)
+
+    year_series = df[df['Investors'].str.contains(name)].groupby('Year')['Amount'].sum()
+
+    st.subheader('YoY Investment')
+    fig4, ax4 = plt.subplots()
+    ax4.plot(year_series.index, year_series.values)
+
+    st.pyplot(fig4)
+
--- app.py
+++ app.py
@@ -2,54 +2,9 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import time
+import Filter_Methods
 
-
-def display_investor_info(name):
-    st.header(name+" Investment Details in startups")
-    st.subheader("Recent Investments")
-    st.dataframe(df[df['Investors'].str.contains(name)][['Date','Startup','Vertical','City','Amount']].head(5))
-    col1, col2 = st.columns(2)
-    with col1:
-        investment_sectors = df[df['Investors'].str.contains(name)].groupby('Vertical')['Amount'].sum()
-        st.subheader('Investments Sectors')
-        fig, ax = plt.subplots()
-        ax.pie(investment_sectors, labels=investment_sectors.index,autopct="%0.01f%%")
-        st.pyplot(fig)
-    with col2:
-        biggest_investments = df[df['Investors'].str.contains(name)].groupby('Startup')['Amount'].sum().sort_values(ascending=False).head()
-        st.subheader("Biggest Investments")
-        fig1, ax1 = plt.subplots()
-        ax1.bar(biggest_investments.index, biggest_investments.values)
-        st.pyplot(fig1)
-
-    col1, col2 = st.columns(2)
-    with col1:
-        investment_types = df[df['Investors'].str.contains(name)].groupby('Investment Type')['Amount'].sum()
-        st.subheader('Investments Rounds')
-        fig2, ax2 = plt.subplots()
-        ax2.pie(investment_types, labels=investment_types.index, autopct="%0.01f%%")
-        st.pyplot(fig2)
-    with col2:
-        investment_cities = df[df['Investors'].str.contains(name)].groupby('City')['Amount'].sum()
-        st.subheader('Investments City')
-        fig3, ax3 = plt.subplots()
-        ax3.pie(investment_cities, labels=investment_cities.index, autopct="%0.01f%%")
-        st.pyplot(fig3)
-
-    year_series = df[df['Investors'].str.contains(name)].groupby('Year')['Amount'].sum()
-
-    st.subheader('YoY Investment')
-    fig4, ax4 = plt.subplots()
-    ax4.plot(year_series.index, year_series.values)
-
-    st.pyplot(fig4)
-
-
-
-
-
-
-
+st.set_page_config(layout='wide', page_title='Startup Analysis Using Pandas')
 df = pd.read_csv("Cleaned_startup.csv")
 df['Date'] = pd.to_datetime(df['Date'])
 df['Year'] = df['Date'].dt.year
@@ -61,24 +16,26 @@ options = st.sidebar.selectbox("Select any criteria ", ['Overall', 'Startups', '
 
 
 if options == 'Startups':
-    st.title("Startup Analysis using Pandas")
+    st.markdown("<h1 style='text-align: center; color: red;'>Coming Soon..</h1>",
+                unsafe_allow_html=True)
     st.sidebar.selectbox("Select Startup", df['Startup'].unique().tolist())
     st.sidebar.button("Click here to Startup Analysis")
 
 
 elif options == 'Investors':
-    st.title("Investors Analysis using Pandas")
+    st.markdown("<h1 style='text-align: center; color: red;'>Investors wise Analysis</h1>",
+                unsafe_allow_html=True)
     investor_name = st.sidebar.selectbox("Select Investor", sorted(set(df['Investors'].str.split(',').sum())))
     investor_button = st.sidebar.button("Click here to Investors Analysis")
     if investor_button:
-        display_investor_info(investor_name)
+        Filter_Methods.display_investor_info(df,investor_name)
 
 
 
 else:
-    st.title("Overall Analysis using Pandas")
-    st.dataframe(df)
-    st.sidebar.button("Click here to Overall Analysis")
+    st.markdown("<h1 style='text-align: center; color: red;'>Overall Analysis of Startups</h1>",
+                unsafe_allow_html=True)
+    Filter_Methods.display_overall_analysis(df)
 
 
 
